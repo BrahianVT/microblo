16f4156db901843144568fb4032a2b8e91a9104c
BrahianVT <pumasemj@hotmail.com>
2025-09-19 22:55:39 -0600

    disable cache pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,5 +67,14 @@ export default defineConfig({
           : [],
       ],
     },
+
+  },
+  // Add this for server-rendered pages
+  server: {
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
   },
 })
